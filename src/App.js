7fee437a6aae848d30473a52a03a116de5a5cccb
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-12-15 14:07:28 -0600

    Initial push with first couple of changes to App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
-
 import FavoriteMovieList from './components/FavoriteMovieList';
+import EditMovieForm from './components/EditMovieForm';
 
 import axios from 'axios';
 
@@ -44,6 +44,7 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm {...props} />
             </Route>
 
             <Route path="/movies/:id">
