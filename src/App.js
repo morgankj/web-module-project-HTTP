477c34643fb3ae1982c7eded64a6fa598e7088f2
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2021-12-15 15:33:11 -0600

    Added in Delete functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
@@ -14,8 +14,10 @@ const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
+  const { push } = useHistory();
+
   useEffect(()=>{
-    axios.get('http://localhost:5001/api/movies')
+    axios.get('http://localhost:9000/api/movies')
       .then(res => {
         setMovies(res.data);
       })
@@ -24,7 +26,13 @@ const App = (props) => {
       });
   }, []);
 
-  const deleteMovie = (id)=> {
+  const deleteMovie = (id) => {
+    axios.delete(`http://localhost:9000/api/movies/${id}`)
+      .then(res => {
+        setMovies(res.data);
+        push('/movies');
+      })
+      .catch(err => console.error(err));
   }
 
   const addToFavorites = (movie) => {
@@ -48,7 +56,7 @@ const App = (props) => {
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie}/>
             </Route>
 
             <Route path="/movies">
